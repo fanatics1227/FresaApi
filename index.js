cd69491691174753a3e9fc44633c7866577996ae
fanatics1227 <eljugador1227@gmail.com>
2019-06-16 14:40:42 -0400

    First version

--- index.js
+++ index.js
@@ -53,9 +53,73 @@ var slap1 = [
     "https://media.giphy.com/media/TGqhg8CXipgzK8UEdr/giphy.gif",
     "https://media.giphy.com/media/INiX4cFXcrWCs/giphy.gif",
     "https://media.giphy.com/media/yt8EKvAPTXNza/giphy.gif"
-  ]
+  ];
 
+  var fusion1 = [
+    "https://media.giphy.com/media/tzF82GBfd4mQ/giphy.gif",
+    "https://media.giphy.com/media/RBbQJq1wFrRC/giphy.gif",
+    "https://cdn.discordapp.com/attachments/563930430009638922/564987019185618944/Baile_de_fusion_Garnet_2.gif",
+    "https://media.giphy.com/media/auvns6y9XRoxW/giphy.gif",
+    "https://media.giphy.com/media/SnEvkITaNG6DS/giphy.gif",
+    "https://media.giphy.com/media/46BoviBstaFl6/giphy.gif",
+    "https://media.giphy.com/media/ZtiQgD6XcqWze/giphy.gif",
+    "https://media.giphy.com/media/P4TqKx6NHyLnO/giphy.gif",
+    "https://media.tenor.com/images/8827cdb5b52f5536a40a5083a7cc4dec/tenor.gif",
+    "https://media1.tenor.com/images/7b43b111b7c22afa5859cd65ed2ae4e1/tenor.gif",
+    "https://media.tenor.com/images/fcc0268b2af5ecfaa81e27a0acbfacbb/tenor.gif",
+    "https://media.tenor.com/images/5ab45d1c56cc734a182fefaaf0975cf1/tenor.gif",
+    "https://media.tenor.com/images/8d13e18759fc83aaf7dbd4396196568a/tenor.gif"
+  ];
+
+  var cry1 = [
+    "https://gifimage.net/wp-content/uploads/2018/11/violet-evergarden-crying-gif-2.gif",
+    "https://gifimage.net/wp-content/uploads/2017/11/gif-anime-llorando-3.gif",
+    "https://gifimage.net/wp-content/uploads/2018/11/zero-two-crying-gif-5.gif",
+    "https://gifimage.net/wp-content/uploads/2018/11/zero-two-crying-gif-5.gif",
+    "https://gifimage.net/wp-content/uploads/2018/11/toradora-taiga-crying-gif-1.gif",
+    "https://gifimage.net/wp-content/uploads/2018/11/umaru-crying-gif-3.gif",
+    "https://gifimage.net/wp-content/uploads/2018/11/toradora-taiga-crying-gif-2.gif",
+    "https://media.giphy.com/media/4NuAILyDbmD16/giphy.gif",
+    "https://media.giphy.com/media/q60nZfZcp5SuY/giphy.gif",
+    "https://media.giphy.com/media/OPU6wzx8JrHna/giphy.gif",
+    "https://media.giphy.com/media/10tIjpzIu8fe0/giphy.gif",
+    "https://media.giphy.com/media/Xqlsn2kLPBquI/giphy.gif",
+    "https://media.giphy.com/media/qscdhWs5o3yb6/giphy.gif",
+    "https://media.giphy.com/media/11TN3gkseh4Vos/giphy.gif",
+    "https://media.giphy.com/media/5cV8AHlDtNo64/giphy.gif",
+    "https://media.giphy.com/media/EHHi29hCF0hlm/giphy.gif",
+    "https://media.giphy.com/media/As8upVH7z3vkQ/giphy.gif",
+    "https://media.giphy.com/media/8YutMatqkTfSE/giphy.gif",
+    "https://media.giphy.com/media/EvqAD0P9gwbOo/giphy.gif",
+    "https://media.giphy.com/media/RUZZqXiGgTyec/giphy.gif",
+    "https://media.giphy.com/media/EvqAD0P9gwbOo/giphy.gif",
+    "https://media.giphy.com/media/12Bpme5pTzGmg8/giphy.gif",
+    "https://media.giphy.com/media/BEob5qwFkSJ7G/giphy.gif",
+    "https://media.giphy.com/media/2rtQMJvhzOnRe/giphy.gif"
+  ];
   
+  var meme1 = [
+    "https://cdn.discordapp.com/attachments/539720786131288064/542042139421442053/Quiz_Kid_20122018215650.jpg",
+    "https://images-ext-1.discordapp.net/external/vTpCThN7yzA_D3T9mIM2u-FRAQ1xVZQNIFjCxtTAHyk/https/cdn.discordapp.com/attachments/491363577576554496/493864873738895422/42111191_338464000223724_5264160514979659776_n.png?width=284&height=300",
+    "https://media.discordapp.net/attachments/529773110761750539/530155641231245313/IMG-20181213-WA0000.jpg",
+    "https://images7.memedroid.com/images/UPLOADED180/5504bc8a25bec.jpeg",
+    "https://images-ext-2.discordapp.net/external/eZeyf3hBdb0ylMyTAuy6bYNsmTRbz2N55eg9KZgpBu0/https/pbs.twimg.com/media/DsS64XQWkAAqnyR.jpg%3Alarge?width=300&height=300",
+    "https://images7.memedroid.com/images/UPLOADED993/57a0f8f2bb9fc.jpeg",
+    "https://images7.memedroid.com/images/UPLOADED191/56bf057ecbf7a.jpeg",
+    "https://images-ext-2.discordapp.net/external/1xPG4bar9Xt7yvJZNn97GDyqCK-CEaHIPvL5x6VJh2k/https/images7.memedroid.com/images/UPLOADED720/5819189543ca4.jpeg?width=318&height=300",
+    "https://images-ext-1.discordapp.net/external/uCyj9VHGCoZNYHk_znFA67fov9g3_zokpCKwhbpKN5c/https/images3.memedroid.com/images/UPLOADED107/59862c45910b1.jpeg?width=400&height=288",
+    "https://media.discordapp.net/attachments/539720786131288064/541374977644167178/21192582_1963089090637838_5614651520797118257_n.jpg?width=331&height=301",
+    "https://cdn.discordapp.com/attachments/563987799305355275/566827145641328650/c803b132848fb50a770e90da8324f2bb.png",
+    "https://i.imgflip.com/32dclm.jpg",
+    "https://cdn.discordapp.com/attachments/550089850519420959/584166415842672661/IMG-20190531-WA0003.jpg",
+    "https://cdn.discordapp.com/attachments/550089850519420959/584166415842672663/image0-232.jpg",
+    "https://cdn.discordapp.com/attachments/550089850519420959/584166416916283402/20190531_172504.JPG",
+    "https://cdn.discordapp.com/attachments/550089850519420959/584166416916283403/images_11.jpg",
+    "https://cdn.discordapp.com/attachments/576845173213102095/584405781290418178/Sin_titulo10.png",
+    "https://cdn.discordapp.com/attachments/581480379748188170/583484063512526858/Screenshot_2019-05-29-21-36-26.png",
+    "https://cdn.dopl3r.com/memes_files/when-veslapalabra-when-entu-examendeingles-yo-te-conosco-PlTSf.jpg"
+  ];
+
   /**
    * @returns {string}
    */
@@ -69,7 +133,22 @@ var slap1 = [
        return hug1[Math.floor(Math.random() * hug1.length)]
    }
 
+   function fusion() {
+     return fusion1[Math.floor(Math.random() * fusion1.length)]
+   }
+
+   function cry() {
+     return cry1[Math.floor(Math.random() * cry1.length)]
+   }
+
+   function meme() {
+     return meme1[Math.floor(Math.random() * meme1.length)]
+   }
+
    module.exports = {
     slap,
-    hug
+    hug,
+    fusion,
+    cry,
+    meme
   };
